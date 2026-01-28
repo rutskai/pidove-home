b694edc6d82762a7d4d503d4407a30eb7127c3a3
Ruth <rcolladogarcia@iessonferrer.net>
2026-01-28 20:42:31 +0100

    Update homepage

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from "../../components/header/header";
+import { PigeonCards } from "../../components/pigeon-cards/pigeon-cards";
 
 @Component({
   selector: 'app-home-page',
-  imports: [Header],
+  imports: [Header, PigeonCards],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
